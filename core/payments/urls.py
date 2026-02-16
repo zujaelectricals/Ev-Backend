314edc91fc0802dbbdd1df6fec7cf17e097ce7eb
toqse <developmenttoqse@gmail.com>
2026-02-16 16:48:57 +0530

    Fix webhook URL to use function-based view instead of class-based view

--- core/payments/urls.py
+++ core/payments/urls.py
@@ -6,7 +6,7 @@ app_name = 'payments'
 urlpatterns = [
     path('create-order/', views.create_order, name='create-order'),
     path('verify/', views.verify_payment, name='verify'),
-    path('webhook/', views.RazorpayWebhookView.as_view(), name='webhook'),
+    path('webhook/', views.webhook, name='webhook'),
     path('create-payout/', views.create_payout, name='create-payout'),
     path('refund/', views.create_refund, name='refund'),
 ]
